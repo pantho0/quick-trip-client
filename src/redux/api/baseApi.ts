0459b99a48343dff291161ecd3aee7f5daaeef0d
pantho0 <panthomashreky6@gmail.com>
2024-12-28 20:35:40 +0600

    fuctionalities added to kick user for invalid refreshtoken

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
-import { setUser } from "../features/auth/authSlice";
+import { logout, setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api",
@@ -15,7 +15,7 @@ const baseQuery = fetchBaseQuery({
 });
 
 const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
+  let result = await baseQuery(args, api, extraOptions);
   if (result?.error?.status === 401) {
     console.log("Sending refresh token");
     const res = await fetch("http://localhost:5000/api/auth/refresh-token", {
@@ -23,14 +23,19 @@ const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
       credentials: "include",
     });
     const data = await res.json();
-    const user = (api.getState() as RootState).auth.user;
-    console.log(data?.data.token);
-    api.dispatch(
-      setUser({
-        user,
-        token: data?.data?.token,
-      })
-    );
+    if (data?.data?.token) {
+      const user = (api.getState() as RootState).auth.user;
+      api.dispatch(
+        setUser({
+          user,
+          token: data?.data?.token,
+        })
+      );
+    } else {
+      api.dispatch(logout());
+    }
+
+    result = await baseQuery(args, api, extraOptions);
   }
   return result;
 };
