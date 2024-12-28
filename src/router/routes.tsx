6c88f1fd72bdc00dc868cef08456bef1208fabc0
pantho0 <panthomashreky6@gmail.com>
2024-12-28 12:39:33 +0600

    dashboard layout added

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -3,12 +3,24 @@ import App from "../App";
 import { globalPaths } from "./global.routes";
 import { routeGenerator } from "../utils/routeGenerator";
 import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+import DashboardLayout from "../components/layouts/DashboardLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: routeGenerator(globalPaths),
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
   },
   {
     path: "/login",
