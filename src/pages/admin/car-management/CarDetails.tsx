010978fbc2563f9347d14373f3cc3c44c97d7876
pantho0 <panthomashreky6@gmail.com>
2024-12-31 00:22:40 +0600

    card design added to show car details

--- src/pages/admin/car-management/CarDetails.tsx
+++ src/pages/admin/car-management/CarDetails.tsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useGetSingleCarQuery } from "../../../redux/features/admin/carManagement.api";
+import { Card, Divider, Flex, Tag } from "antd";
+const { Meta } = Card;
 
 const CarDetails = () => {
   const { id } = useParams();
@@ -8,17 +10,51 @@ const CarDetails = () => {
   console.log(data);
 
   return (
-    <>
-      Car Name : <h1>{data?.name}</h1>
-      Hourly Price : <p>{data?.pricePerHour}</p>
-      Current Status : <p>{data?.status}</p>
-      Description : <p>{data?.description}</p>
-      isElectric : <p>{data?.isElectric}</p>
-      Features :{" "}
-      {data?.features.map((f) => (
-        <p>{f}</p>
-      ))}
-    </>
+    <Flex justify="center" align="center">
+      <Card
+        hoverable
+        cover={
+          <img
+            style={{ width: "100%", height: "200x" }}
+            alt="example"
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          />
+        }
+      >
+        <Meta title={data?.name} description={data?.description} />
+        <div
+          style={{
+            border: "2px solid #f0f0f0",
+            borderRadius: "15px",
+            padding: "10px",
+            margin: "15px",
+            fontSize: "14px",
+          }}
+        >
+          <p>Hourly Price : {data?.pricePerHour}</p>
+          <p>
+            Current Status :{" "}
+            <Tag color={data?.status === "available" ? "green" : "yellow"}>
+              {data?.status.toUpperCase()}
+            </Tag>{" "}
+          </p>
+          <p>isElectric : {data?.isElectric}</p>
+          <div>
+            <p style={{ textAlign: "center", fontSize: "20px", lineHeight: 0 }}>
+              Features
+            </p>
+            <p style={{ textAlign: "center" }}>
+              <br />
+              {data?.features.map((f) => (
+                <Tag style={{ fontSize: "14px" }}>
+                  {f} <br />
+                </Tag>
+              ))}
+            </p>
+          </div>
+        </div>
+      </Card>
+    </Flex>
   );
 };
 
