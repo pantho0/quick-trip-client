c3469ea6f841bb44e5e0c99b009b5fba1df0c10b
pantho0 <panthomashreky6@gmail.com>
2025-01-02 15:43:34 +0600

    Flex comp used for banner headline

--- src/components/Home-Items/Banner.tsx
+++ src/components/Home-Items/Banner.tsx
@@ -1,4 +1,14 @@
-import { Button, Carousel, Col, Form, Image, Input, Row, Space } from "antd";
+import {
+  Button,
+  Carousel,
+  Col,
+  Flex,
+  Form,
+  Image,
+  Input,
+  Row,
+  Space,
+} from "antd";
 import "../../styles/banner.css";
 
 const Banner = () => {
@@ -8,45 +18,42 @@ const Banner = () => {
   return (
     <Row>
       <Col span={24} md={{ span: 12 }}>
-        <Row
-          style={{
-            display: "flex",
-            alignContent: "center",
-            height: "100vh",
-          }}
+        <Flex
+          align="flex-start"
+          justify="center"
+          vertical
+          style={{ height: "100vh" }}
         >
-          <Space direction="vertical" size={"middle"}>
-            <h1 className="responsive-title">
-              QuickTrip:Your Journey, Simplified
-            </h1>
-            <h4 className="responsive-subtitle">
-              Effortless Car Reservations for Every Destination
-            </h4>
-            <p className="responsive-paragraph">
-              Quick Trip makes finding the perfect car for your journey a
-              breeze. Simply select your pickup and drop-off locations, and let
-              us connect you with the best options available. Wherever you're
-              headed, Quick Trip ensures a smooth ride every time.
-            </p>
+          <h1 className="responsive-title">
+            QuickTrip:Your Journey, Simplified
+          </h1>
+          <h4 className="responsive-subtitle">
+            Effortless Car Reservations for Every Destination
+          </h4>
+          <p className="responsive-paragraph">
+            Quick Trip makes finding the perfect car for your journey a breeze.
+            Simply select your pickup and drop-off locations, and let us connect
+            you with the best options available. Wherever you're headed, Quick
+            Trip ensures a smooth ride every time.
+          </p>
 
-            <div
-              style={{
-                display: "flex",
-                gap: "16px",
-              }}
-            >
-              <Form layout="vertical" className="responsive-form">
-                <Form.Item name={"From"} label={"From"} style={{ flex: 1 }}>
-                  <Input />
-                </Form.Item>
-                <Form.Item name={"To"} label={"To"} style={{ flex: 1 }}>
-                  <Input />
-                </Form.Item>
-                <Button>Search Car</Button>
-              </Form>
-            </div>
-          </Space>
-        </Row>
+          <div
+            style={{
+              display: "flex",
+              gap: "16px",
+            }}
+          >
+            <Form layout="vertical" className="responsive-form">
+              <Form.Item name={"From"} label={"From"} style={{ flex: 1 }}>
+                <Input />
+              </Form.Item>
+              <Form.Item name={"To"} label={"To"} style={{ flex: 1 }}>
+                <Input />
+              </Form.Item>
+              <Button>Search Car</Button>
+            </Form>
+          </div>
+        </Flex>
       </Col>
       <Col span={24} md={{ span: 12 }}>
         <Carousel afterChange={onChange} autoplay>
