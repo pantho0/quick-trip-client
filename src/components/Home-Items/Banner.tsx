7ed5e4fe0b49e67421c237201bbb0b160c714323
pantho0 <panthomashreky6@gmail.com>
2025-01-02 15:55:07 +0600

    updated image carausol design

--- src/components/Home-Items/Banner.tsx
+++ src/components/Home-Items/Banner.tsx
@@ -1,14 +1,8 @@
-import {
-  Button,
-  Carousel,
-  Col,
-  Flex,
-  Form,
-  Image,
-  Input,
-  Row,
-  Space,
-} from "antd";
+import { Button, Carousel, Col, Flex, Form, Image, Input, Row } from "antd";
+import tesla from "../../assets/images/tesla.jpg";
+import bmw from "../../assets/images/bmw.jpg";
+import audi from "../../assets/images/audi.jpg";
+import ford from "../../assets/images/ford.jpg";
 import "../../styles/banner.css";
 
 const Banner = () => {
@@ -56,26 +50,22 @@ const Banner = () => {
         </Flex>
       </Col>
       <Col span={24} md={{ span: 12 }}>
-        <Carousel afterChange={onChange} autoplay>
-          <div>
-            <Image
-              className="reponsive-bannerImage"
-              src="https://plus.unsplash.com/premium_photo-1664303847960-586318f59035?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            />
-          </div>
-          <div>
-            <Image
-              className="reponsive-bannerImage"
-              src="https://images.unsplash.com/photo-1524095838546-2d9b8d00f212?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            />
-          </div>
-          <div>
-            <Image
-              className="reponsive-bannerImage"
-              src="https://images.unsplash.com/photo-1544468607-e7b3e848ff87?q=80&w=1550&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            />
-          </div>
-        </Carousel>
+        <Flex vertical justify="center" style={{ height: "100vh" }}>
+          <Carousel afterChange={onChange} autoplay>
+            <div>
+              <Image className="reponsive-bannerImage" src={tesla} />
+            </div>
+            <div>
+              <Image className="reponsive-bannerImage" src={bmw} />
+            </div>
+            <div>
+              <Image className="reponsive-bannerImage" src={audi} />
+            </div>
+            <div>
+              <Image className="reponsive-bannerImage" src={ford} />
+            </div>
+          </Carousel>
+        </Flex>
       </Col>
     </Row>
   );
