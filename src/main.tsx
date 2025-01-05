6ddb48f6b4dd3abb0a9ea1373119e6233bc01fa7
pantho0 <panthomashreky6@gmail.com>
2025-01-05 20:39:10 +0600

    themes controller providers are added

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,17 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { Toaster } from "sonner";
+import { ConfigProvider } from "antd";
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Toaster />
-        <RouterProvider router={router} />
+        <ConfigProvider theme={theme}>
+          <RouterProvider router={router} />
+        </ConfigProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
