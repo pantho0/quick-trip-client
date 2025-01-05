22a969360c859423a77750f444e6cf8f69b63a4b
pantho0 <panthomashreky6@gmail.com>
2025-01-05 14:11:08 +0600

    hambarger added with drawer for small device navigation

--- src/components/layouts/Mainlayout.tsx
+++ src/components/layouts/Mainlayout.tsx
@@ -1,16 +1,31 @@
-import { Button, Layout, Menu, theme } from "antd";
+import { Button, Drawer, Layout, Menu, theme } from "antd";
 import { Link, Outlet } from "react-router-dom";
 import { navLinkGenerator } from "../../utils/navLinkGenerator";
 import { globalPaths } from "../../router/global.routes";
 import FooterSection from "../shared/FooterSection";
+import { useState } from "react";
+import { MenuOutlined } from "@ant-design/icons";
+import "./layout.css";
 
 const { Header, Content } = Layout;
 
 const items = navLinkGenerator(globalPaths);
+
 const Mainlayout = () => {
+  const [visible, setVisible] = useState(false);
+
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
   return (
     <Layout>
       <Header
@@ -24,21 +39,57 @@ const Mainlayout = () => {
           Logo
         </div>
         <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+          <MenuOutlined
+            style={{
+              color: "wheat",
+              fontSize: "24px",
+              display: "none",
+            }}
+            onClick={showDrawer}
+            className="hamburger-menu"
+          />
+
           <Menu
             theme="dark"
             mode="horizontal"
             defaultSelectedKeys={["1"]}
             items={items}
-            style={{ minWidth: 0 }}
+            className="desktop-menu"
           />
-          <Link to={"/login"}>
+          <Link className="desktop-menu" to={"/login"}>
             <Button>Log in</Button>
           </Link>
-          <Link to="/signup">
+          <Link className="desktop-menu" to="/signup">
             <Button>Sign Up</Button>
           </Link>
         </div>
       </Header>
+      <Drawer
+        style={{ backgroundColor: "#001529" }}
+        title="Menu"
+        placement="left"
+        onClose={onClose}
+        open={visible}
+        width={250}
+      >
+        <Menu
+          mode="inline"
+          theme="dark"
+          defaultSelectedKeys={["1"]}
+          items={items}
+          onClick={onClose}
+        />
+        <div style={{ width: "100%", marginTop: "20px" }}>
+          <Link className="hamburger-menu" to={"/login"}>
+            <Button style={{ width: "100%", marginBottom: "15px" }}>
+              Log in
+            </Button>
+          </Link>
+          <Link className="hamburger-menu" to="/signup">
+            <Button style={{ width: "100%" }}>Sign Up</Button>
+          </Link>
+        </div>
+      </Drawer>
       <Content style={{ padding: "0 48px" }}>
         <div
           style={{
