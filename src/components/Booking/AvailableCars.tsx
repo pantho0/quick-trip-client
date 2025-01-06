b0effddb67e58776f3e8a2861cd323749d1e2d07
pantho0 <panthomashreky6@gmail.com>
2025-01-06 13:08:54 +0600

    consistant git card height and align elements in a line

--- src/components/Booking/AvailableCars.tsx
+++ src/components/Booking/AvailableCars.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import "../../styles/globalButton.css";
 import Loader from "../shared/Loader";
 import { useState } from "react";
+import "./availableCar.css";
 
 const AvailableCars = () => {
   const [page, setPage] = useState(1);
@@ -46,49 +47,58 @@ const AvailableCars = () => {
         {cars?.data?.map((car) => (
           <Col key={car._id} xs={24} sm={12} md={8} lg={6}>
             <Card
+              className="ant-card-body"
               loading={isFetching && isLoading}
               hoverable
-              style={{ width: "100%" }}
+              style={{ width: "100%", minHeight: "500px" }}
               cover={
                 <img
                   alt="example"
-                  style={{ objectFit: "fill" }}
+                  style={{
+                    objectFit: "cover",
+                    width: "100%",
+                    height: "200px",
+                  }}
                   src={car?.images}
                 />
               }
             >
-              <Meta
-                title={car?.name}
-                description={`${car?.description.slice(0, 50)}...`}
-              />
-              <p style={{ marginTop: "3px" }}>
-                Hourly Price : ${car?.pricePerHour}
-              </p>
-              <p style={{ marginTop: "3px" }}>
-                Features :<br />
-                {car?.features?.slice(0, 3)?.map((f) => (
-                  <Tag color="green" style={{ margin: "2px" }} key={f}>
-                    {f}
-                  </Tag>
-                ))}
-              </p>
-              <Link to={`/user/create-booking/${car?._id}`}>
-                <Flex
-                  justify="center"
-                  style={{ marginTop: "10px", width: "100%" }}
-                >
-                  <Button
-                    type="primary"
-                    disabled={car?.status === "booked"}
-                    className={`${
-                      car?.status === "available" ? "globalButton" : ""
-                    } `}
-                    style={{ width: "100%" }}
+              <div className="card-content">
+                <Meta
+                  title={car?.name}
+                  description={`${car?.description.slice(0, 50)}...`}
+                />
+                <p style={{ marginTop: "3px" }}>
+                  Hourly Price : ${car?.pricePerHour}
+                </p>
+                <p style={{ marginTop: "3px" }}>
+                  Features :<br />
+                  {car?.features?.slice(0, 3)?.map((f) => (
+                    <Tag color="green" style={{ margin: "2px" }} key={f}>
+                      <li>{f}</li>
+                    </Tag>
+                  ))}
+                </p>
+                <Link to={`/user/create-booking/${car?._id}`}>
+                  <Flex
+                    justify="center"
+                    style={{ marginTop: "10px", width: "100%" }}
                   >
-                    {`${car?.status === "available" ? "Book Now" : "Booked  "}`}
-                  </Button>
-                </Flex>
-              </Link>
+                    <Button
+                      type="primary"
+                      disabled={car?.status === "booked"}
+                      className={`${
+                        car?.status === "available" ? "globalButton" : ""
+                      } `}
+                      style={{ width: "100%" }}
+                    >
+                      {`${
+                        car?.status === "available" ? "Book Now" : "Booked  "
+                      }`}
+                    </Button>
+                  </Flex>
+                </Link>
+              </div>
             </Card>
           </Col>
         ))}
